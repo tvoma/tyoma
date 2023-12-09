d8f0ab0787dffe90618680defd5b33a50aab190d
Tyoma <hello@tyoma.co>
2023-12-09 15:37:36 +0100

    feat: home page

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,17 @@
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata = {
   	title: 'Tyoma Portfolio',
-  	description: 'French web developper'
+  	description: 'French web developer'
 }
 
 export default function RootLayout({ children }) {
+	const classList = [GeistMono.variable, GeistSans.variable].join(' ')
+
 	return (
-		<html lang="en">
+		<html lang="en" className={ classList }>
 			<body>{ children }</body>
 		</html>
 	)
--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import { IconBrandGithub } from "@tabler/icons-react";
+
 export default function Home() {
-    return (<></>)
+    return (
+        <div>
+            <h1>&gt; Welcome on board</h1>
+            <p>&gt; This is the portfolio of a french web developer. His pseudonym is Tyoma.</p>
+            <a target="_blank" href="https://github.com/tvoma">
+                <IconBrandGithub /> <i>tvoma</i>
+            </a>
+        </div>
+    )
 }

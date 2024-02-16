4ef658aceb5b1a6e77dfc67c84d713e97914f0d0
Tyoma <hello@tyoma.co>
2024-02-16 18:21:21 +0100

    feat: content style

--- components/content/index.js
+++ components/content/index.js
@@ -4,8 +4,9 @@ import styles from './styles.module.css'
 function Content() {
     return (
         <div className={ styles.container }>
-            <h1>&gt; Welcome on board</h1>
-            <p>&gt; This is the portfolio of a french web developer. His pseudonym is Tyoma.</p>
+            <p className={ styles.gps }>N 47°19&rsquo;17&rdquo; E 5°2&rsquo;29&rdquo;</p>
+            <h1>Welcome on board</h1>
+            <p>This is the portfolio of a french web developer. His pseudonym is Tyoma.</p>
             <a target="_blank" href="https://github.com/tvoma">
                 <IconBrandGithub /> <i>tvoma</i>
             </a>

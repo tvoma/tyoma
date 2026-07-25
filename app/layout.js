11716b69efff50624a6de8c5612cfeb634bd197c
Tyoma <hello@tyoma.co>
2026-07-25 12:00:49 +0200

    feat: add description

--- app/layout.js
+++ app/layout.js
@@ -3,15 +3,15 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata = {
-  	title: 'Tyoma\'s Portfolio',
-  	description: 'French web developer'
+  	title: 'Tyoma\'s Portfolio - Développeur Fullstack JavaScript',
+  	description: 'Développeur Fullstack JavaScript / Typescript localisé en France. Spécialiste React / Next.js. Maitrise Node.js, Prisma, API Rest, SEO, accessibilité web, .',
 }
 
 export default function RootLayout({ children }) {
 	const classList = [GeistMono.variable, GeistSans.variable].join(' ')
 
 	return (
-		<html lang="en" className={ classList }>
+		<html lang='fr' className={ classList }>
 			<body>{ children }</body>
 		</html>
 	)
--- components/content/index.js
+++ components/content/index.js
@@ -1,15 +1,26 @@
-import { IconBrandGithub } from "@tabler/icons-react"
+import { IconAt, IconBrandGithub, IconBrandLinkedin, IconMail } from "@tabler/icons-react"
 import styles from './styles.module.css'
 
 function Content() {
     return (
         <div className={ styles.container }>
-            <p className={ styles.gps }>48°33&rsquo;14&rdquo;N 0°25&rsquo;22&rdquo;E</p>
+            <p className={ styles.gps }>49°11&rsquo;10&rdquo;N 0°21&rsquo;46&rdquo;W</p>
             <h1>developer.js</h1>
-            <p>Votre futur développeur favori.</p>
+            <p></p>
+            <p>&gt; Développeur fullstack Javascript &bull; Votre futur développeur favori.</p>
+            <p className='text-light'>J&apos;interviens sur des applications web, de l&apos;interface à l&apos;API, avec une attention particulière portée à la logique métier de chaque projet.</p>
+            <p></p>
             <a target="_blank" href="https://github.com/tvoma">
-                <IconBrandGithub /> <i>tvoma</i>
+                <IconBrandGithub /> <i>Github</i>
             </a>
+            <a target="_blank" href="https://www.linkedin.com/in/tvoma/">
+                <IconBrandLinkedin /> <i>LinkedIn</i>
+            </a>
+            <p className={ styles.callToAction }>Un projet ? Parlons-en !
+                <a href="mailto:hello@tyoma.co">
+                    <IconAt /> <i>hello@tyoma.co</i>
+                </a>
+            </p>
         </div>
     )
 }
